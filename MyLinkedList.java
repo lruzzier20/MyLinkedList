cd573e81dd7802b0fdee3464a440515462c7601a
Logan <lruzzier20@stuy.edu>
2021-01-07 21:04:21 -0500

    added add()

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -9,4 +9,13 @@ public class MyLinkedList{
   public int size(){
     return size;
   }
+
+  public boolean add(String value){
+    Node a = new Node(value)
+    if(size==0){start=a; size=1; return true;}
+    end.setNext(a);
+    a.setPrev(end);
+    end=a;
+    return true;
+  }
 }
