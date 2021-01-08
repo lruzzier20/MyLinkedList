632da879faf471470697a5c673be750581ecef42
Logan <lruzzier20@stuy.edu>
2021-01-07 20:57:36 -0500

    added size()

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -5,4 +5,8 @@ public class MyLinkedList{
   public MyLinkedList(){
     super();
   }
+
+  public int size(){
+    return size;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester{
   public static void main(String[] args){
     MyLinkedList a = new MyLinkedList();
+    System.out.println(a.size());
   }
 }
